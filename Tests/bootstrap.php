d9d92c66128caebc0323b9634b1855176bad90f1
Xavier HAUSHERR <xavier.hausherr@wikiogroup.com>
2011-12-21 00:37:10 +0100

    Repair Test

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -10,7 +10,7 @@ $loader->register();
 
 spl_autoload_register(function($class)
 {
-    if (0 === strpos($class, 'Overblog\\WsClientBundle\\')) {
+    if (0 === strpos($class, 'Overblog\\ThriftBundle\\')) {
         $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
         require_once __DIR__.'/../'.$path;
         return true;
