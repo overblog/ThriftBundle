522197cbc70bace81277608d7bfa1ec8e2139b16
Xavier HAUSHERR <xavier.hausherr@wikiogroup.com>
2012-09-10 15:10:04 +0200

    Add uses

--- Transport/THttpClient.php
+++ Transport/THttpClient.php
@@ -3,6 +3,9 @@
 namespace Overblog\ThriftBundle\Transport;
 
 use Thrift\Transport\THttpClient as Client;
+use Thrift\Transport\TTransport;
+use Thrift\Exception\TTransportException;
+use Thrift\Factory\TStringFuncFactory;
 
 /**
  * Description of THttpClient
