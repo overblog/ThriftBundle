cd96edb4ff0f5615e591fc912b1d6c2c2997e264
Xavier HAUSHERR <xavier.hausherr@ebuzzing.com>
2013-01-14 15:27:51 +0100

    Compiler throw now an exception during Warmup (cherry picked from commit e18811142526b10aa05108a739914f1b5d9d2c59)

---
+++ Exception/CompilerException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Overblog\ThriftBundle\Exception;
+
+/**
+ * This exception is throw when a compilation error is encountered
+ *
+ * @author Xavier HAUSHERR <xavier.hausherr@ebuzzing.com>
+ */
+class CompilerException extends \Exception {}
