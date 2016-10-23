651196539921e101af16de2243897d8f7a533602
Jeremiah VALERIE <jeremiah@over-blog.com>
2016-10-23 07:51:09 +0200

    Cleanup phing build

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,19 +1,3 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/ClassLoader.php';
-
-use Symfony\Component\ClassLoader\ClassLoader;
-
-$loader = new ClassLoader();
-$loader->addPrefix('Symfony', $_SERVER['SYMFONY']);
-$loader->addPrefix('Thrift', $_SERVER['THRIFT']);
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'Overblog\\ThriftBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        require_once __DIR__.'/../'.$path;
-        return true;
-    }
-});
+$loader = require __DIR__.'/../vendor/autoload.php';
